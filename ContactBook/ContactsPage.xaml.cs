e4bca0e0a7061633ebcc27239ed4712907f36b05
JuneCho23 <junecho.ucb@gmail.com>
2017-08-30 17:46:20 -0700

    added sqlite-nel-pcl package

---
+++ ContactBook/ContactsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using SQLite;
+
+namespace ContactBook
+{
+    public partial class ContactsPage : ContentPage
+    {
+        private ObservableCollection<Contact> _contacts;
+        private SQLiteAsyncConnection _connection;
+
+        public ContactsPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void OnDeleteContact(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
