135a40f86279df63ce9fc1030c657190e2108b44
JuneCho23 <junecho.ucb@gmail.com>
2017-08-30 17:35:37 -0700

    DataTemplate added

--- ContactBook/obj/Debug/ContactBook.ContactPage.xaml.g.cs
+++ ContactBook/obj/Debug/ContactBook.ContactPage.xaml.g.cs
@@ -15,8 +15,12 @@ namespace ContactBook {
     public partial class ContactPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.ListView contactsListView;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(ContactPage));
+            contactsListView = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "contactsListView");
         }
     }
 }
