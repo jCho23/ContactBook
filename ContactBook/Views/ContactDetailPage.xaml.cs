d70dcdf8bbff8c69a357c7e9ae5cd6d77d0634a4
JuneCho23 <junecho.ucb@gmail.com>
2017-08-30 19:13:27 -0700

    finished public EventHandlers

--- ContactBook/Views/ContactDetailPage.xaml.cs
+++ ContactBook/Views/ContactDetailPage.xaml.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using SQLite;
 
 namespace ContactBook.Views
 {
     public partial class ContactDetailPage : ContentPage
     {
-        public ContactDetailPage()
+		public event EventHandler<Contact> ContactAdded;
+        public event EventHandler<Contact> ContactUpdated;
+
+        private SQLiteAsyncConnection _connection;
+
+		public ContactDetailPage(Contact contact)
         {
+
+
             InitializeComponent();
         }
     }
