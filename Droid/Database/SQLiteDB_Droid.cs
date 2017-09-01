7b869e78759b64dd465faeb6a78422ab9557c6dc
JuneCho23 <junecho.ucb@gmail.com>
2017-08-31 20:34:11 -0700

    finished ContactBook.Droid.Database

---
+++ Droid/Database/SQLiteDB_Droid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+using SQLite;
+using ContactBook.Droid.Database;
+using ContactBook.DataBase;
+using System.IO;
+
+[assembly: Dependency(typeof(SQLiteDB_Droid))]
+
+namespace ContactBook.Droid.Database                 
+
+{
+    public class SQLiteDB_Droid : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
