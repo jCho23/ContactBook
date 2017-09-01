a84416447465273b1eadb3d93cab2162c8784014
JuneCho23 <junecho.ucb@gmail.com>
2017-08-31 20:18:55 -0700

    Created Database folder for iOS

---
+++ iOS/SQLiteDB_IOS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ContactBook.DataBase;
+using SQLite;
+
+using Xamarin.Forms;
+using ContactBook.iOS;
+
+[assembly: Dependency(typeof(SQLiteDb_iOS))]
+namespace ContactBook.iOS
+{
+    public class SQLiteDb_iOS :ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            //
+        }
+    }
+}
