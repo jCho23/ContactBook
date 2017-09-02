f5914c9aa5932d8b0bf441d7d11d8b06d97224a0
JuneCho23 <junecho.ucb@gmail.com>
2017-09-01 18:43:20 -0700

    finished BasePage Tests

---
+++ Xamarin.UITest/Pages/BasePage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Xamarin.UITest.Pages
+{
+	public abstract class BasePage
+	{
+		protected readonly IApp App;
+		protected readonly bool OnAndroid;
+		protected readonly bool OniOS;
+
+		protected BasePage(IApp app, Platform platform)
+		{
+			App = app;
+
+			OnAndroid = platform == Platform.Android;
+			OniOS = platform == Platform.iOS;
+		}
+	}
+}
