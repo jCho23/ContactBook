3a03f3a25801a37a31d2ad99885b12f4d4467b26
JuneCho23 <junecho.ucb@gmail.com>
2017-09-01 21:18:39 -0700

    Restored files

--- Xamarin.UITest/Pages/ContactsPage.cs
+++ Xamarin.UITest/Pages/ContactsPage.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
 namespace Xamarin.UITest.Pages
 {
-    public class ContactsPage
+    public class ContactsPage : BasePage
     {
+		readonly Query _goButton;
+		readonly Query _textEntry;
+		readonly Query _textLabel;
+
+
         public ContactsPage()
         {
         }
