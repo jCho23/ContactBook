5545dd89134d134851cfc8771422ba7e9eac0f38
JuneCho23 <junecho.ucb@gmail.com>
2017-09-01 18:41:30 -0700

    CleanUp

---
+++ Xamarin.UITest/Tests/BaseTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Xamarin.UITest.Tests
+
+{
+	[TestFixture(Platform.Android)]
+	[TestFixture(Platform.iOS)]
+
+	public abstract class BaseTest
+	{
+		protected IApp App;
+		protected Platform Platform;
+
+        protected ContactsPage ContactsPage;
+        protected ContactDetailPage ContactDetailPage;
+	
+
+		protected BaseTest(Platform platform) => Platform = platform;
+
+		[SetUp]
+		virtual public void BeforeEachTest()
+		{
+			App = AppInitializer.StartApp(Platform);
+			App.Screenshot("App Launched");
+           
+            ContactsPage = new ContactsPage(App, Platform);
+            ContactDetailPage = new ContactDetailPage(App, Platform);
+		
+			//LoginPage.WaitForLoginScreen();
+		}
+	}
+}
+
