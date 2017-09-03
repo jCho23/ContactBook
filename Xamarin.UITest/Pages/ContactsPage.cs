4f4581634a4e4059380828c4d4d9c487d538076c
JuneCho23 <junecho.ucb@gmail.com>
2017-09-02 22:28:55 -0700

    CleanUp

--- Xamarin.UITest/Pages/ContactsPage.cs
+++ Xamarin.UITest/Pages/ContactsPage.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+using ContactBook.Shared.Constants;
 
 using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
 
@@ -6,13 +12,19 @@ namespace Xamarin.UITest.Pages
 {
     public class ContactsPage : BasePage
     {
-		readonly Query _AddButton;
+		readonly Query _addButton;
 
-        readonly Query _AddButtonUsingID;
+        readonly Query _addButtonUsingID;
 
         public ContactsPage(IApp app, Platform platform) : base(app, platform)
-        { 
-        
+        {
+            _addButtonUsingID = x => x.Marked(AutomationIdConstants.AddButton);
         }
+    }
+
+    public void TappedAddButton()
+    {
+        App.Tap(_addButtonUsingID);
+
     }
 }
