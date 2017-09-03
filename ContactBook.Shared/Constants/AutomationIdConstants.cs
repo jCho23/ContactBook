8f2598bc698599c1f4ff9d84431a5d2718bd6dc9
JuneCho23 <junecho.ucb@gmail.com>
2017-09-02 22:16:02 -0700

    added regions

--- ContactBook.Shared/Constants/AutomationIdConstants.cs
+++ ContactBook.Shared/Constants/AutomationIdConstants.cs
@@ -2,15 +2,17 @@
 {
     public static class AutomationIdConstants
     {
-        #region Contacts Page
+        #region ContactsPage
         public const string AddButton = "AddButton";
         #endregion
 
+        #region ContactDetailsPage
         public const string FirstName = "{Binding FirstName}";
         public const string LastName = "{Binding LastName}";
         public const string Phone = "{Binding Phone}";
         public const string Email = "{Binding Email}";
         public const string IsBlocked = "{Binding IsBlocked}";
         public const string OnSave = "{Binding OnSave}";
+        #endregion
     }
 }
