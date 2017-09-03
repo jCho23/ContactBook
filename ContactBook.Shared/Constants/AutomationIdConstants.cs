b4400f1b231f334348bf65e645e2120754e9b6fb
JuneCho23 <junecho.ucb@gmail.com>
2017-09-02 22:14:56 -0700

    finished Constants

--- ContactBook.Shared/Constants/AutomationIdConstants.cs
+++ ContactBook.Shared/Constants/AutomationIdConstants.cs
@@ -6,9 +6,11 @@
         public const string AddButton = "AddButton";
         #endregion
 
-        public const string FirstName = "FirstName";
-        public const string 
-
-
+        public const string FirstName = "{Binding FirstName}";
+        public const string LastName = "{Binding LastName}";
+        public const string Phone = "{Binding Phone}";
+        public const string Email = "{Binding Email}";
+        public const string IsBlocked = "{Binding IsBlocked}";
+        public const string OnSave = "{Binding OnSave}";
     }
 }
